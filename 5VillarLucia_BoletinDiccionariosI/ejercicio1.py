3177b0e47388f9c15d82757dc97c2408a86659f5
luciavr23 <lucia.villar.rodriguez.alu@iesjulioverne.es>
2025-01-31 01:11:11 +0100

    30.01.25

--- 5VillarLucia_BoletinDiccionariosI/ejercicio1.py
+++ 5VillarLucia_BoletinDiccionariosI/ejercicio1.py
@@ -4,6 +4,7 @@
 
 cadena=input("Intruduzca una cadena / 0 para salir: ")
 
+abecedario={"a":0,"b":0,"c":0,"d":0,"e":0,"f":0,"g":0,"h":0,"i":0,"j":0,"k":0,"l":0,"m":0,"n":0,"ñ":0,"o":0,"p":0,"q":0,"r":0,"s":0,"t":0,"u":0,"v":0,"w":0,"x":0,"y":0,"z":0}
 arrayCadenas=[]
 #funcion que recorra los char de cada cadena, los cuente y los meta en un diccionario como clave valor
 while(cadena!="0"):
